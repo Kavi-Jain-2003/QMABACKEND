07e4ff5fed792eb8e87b878443a080543d2675cf
Kavi-Jain-2003 <kavijain220803@gmail.com>
2026-04-18 10:57:30 +0530

    changes 22

--- measurement-service/src/main/java/com/app/quantitymeasurement/config/SwaggerConfig.java
+++ measurement-service/src/main/java/com/app/quantitymeasurement/config/SwaggerConfig.java
@@ -2,11 +2,8 @@ package com.app.quantitymeasurement.config;
 
 import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
-import io.swagger.v3.oas.models.servers.Server;
 import io.swagger.v3.oas.models.security.SecurityRequirement;
 import io.swagger.v3.oas.models.security.SecurityScheme;
-
-import java.util.List;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -17,7 +14,6 @@ public class SwaggerConfig {
     @Bean
     public OpenAPI customOpenAPI() {
         return new OpenAPI()
-            .servers(List.of(new Server().url("http://localhost:8080")))
             .addSecurityItem(new SecurityRequirement().addList("bearerAuth"))
             .components(new Components()
                 .addSecuritySchemes("bearerAuth",
